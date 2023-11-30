1c4472ec086625b40a0ac675f7f682ed20eb532b
nafasTK <148532651+nafasTK@users.noreply.github.com>
2023-11-30 09:59:46 -0800

    Add files via upload

--- gol ya pooch.py
+++ gol ya pooch.py
@@ -1,29 +1,67 @@
-#gol ya pooch
-print ("This is gol ya pooch game")
-name = input("please enter your name: ")
+#rock - paper - scissors
+b = True
 
-print ("left : 1")
-print ("right : 2")
+name1 = input("please enter your name : ")
+name2 = "computer"
+
+a = int(input("how many rounds do you want to play : "))
+
+player1_score = 0
+player2_score = 0
+
+print(" ")
+print("left")
+print("right")
+print(" ")
+
+while b == True :
+
+    import random
+   
+    randomNumber = random.randint(0,1)
+    computerMove = "a"
+
+    if randomNumber == 0:
+        computerMove = "left"
+    elif randomNumber == 1:
+        computerMove = "right"
+
+    player1 = input(f"{name1} please make your move : ")
+    print(" ")
+    print (f"{name1} : I guess {player1} is gol ")
+    print(f"{name2} : {computerMove} is gol ")
+    player2 = computerMove
+    print(" ")
+    
+    if player1 == "left" and player2 == "right":
+        print ("pooch !")
+        print(" ")
+        player2_score = player2_score + 1
+
+    elif player1 == "right" and player2 == "left":
+        print("pooch !")
+        print(" ")
+        player2_score = player2_score + 1
+
+    elif player1 == player2:
+        print ("gol !")
+        print(" ")
+        player1_score = player1_score + 1
+
+    else:
+        print(" ")
+        print("something went wrong !")
+        print(" ")
+
+    if player1_score == a or player2_score == a :
+        b = False
+        if player1_score == a :
+            print(" ")
+            print(f"{name1} win the game !")
+        elif player2_score == a :
+            print(" ")
+            print(f"{name2} win the game !")
+        break
 
 
-import random
-rand = (random.randint(1,2))
-
-if rand == 1 :
-    player = int(input())
-    print (rand)
-    if player == 1 :
-        print ("gol")
-        print ("you win")
-    if player == 2 :
-        print ("pooch")
-        print ("you lose")
-if rand == 2 :
-    player = int(input())
-    print (rand)
-    if player == 2 :
-        print ("gol")
-        print ("you win")
-    if player == 1 :
-        print ("pooch")
-        print ("you lose")
+input ()
